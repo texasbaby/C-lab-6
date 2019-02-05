7615f4dba6efd852f0145c8e337e3cbd676ea9da
Пользователь <mdylll@mail.ru>
2019-02-05 15:24:23 +0300

    relis 1

--- src/main8.cpp
+++ src/main8.cpp
@@ -3,9 +3,13 @@
 
 int main(int argc, char* argv[])
 {
-	char *buf = argv[0];
-	//char buf[] = "(((7-1)/(4+2))-9)";
-	printf("%d\n", eval(buf));
-
+	if (argc < 2)
+		puts("too few arguments");
+	else
+	{
+		char *buf = argv[1];
+		//char buf[] = "(((7-1)/(4+2))-9)";
+		printf("%d\n", eval(buf));
+	}
 	return 0;
 }
