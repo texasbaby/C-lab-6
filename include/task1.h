ce9af187e417662e7f509966121bed65b8a38e95
Victoria Alekseeva <johndoe@example.comalvi813@yandex.ru>
2019-02-10 11:43:26 +0300

    was added header files

---
+++ include/task1.h
@@ -0,0 +1,4 @@
+#define M 80
+
+void drawFractal(char(*arr)[M], int x, int y, int size);
+void printMatrix(char(*arr)[M]);
---
+++ include/task8.h
@@ -0,0 +1,7 @@
+#define SIZE 100
+
+int eval(char *buf);
+char partition(char *buf, char *expr1, char *expr2);
+int isPar(char sym);
+int isSign(char sym);
+int isNum(char sym);
