56b972367b3b7a3c64f3e6eebb8ac2fda15890ec
Alex Sadikov <sav1982@inbox.ru>
2019-02-17 14:20:43 +0300

    п4

--- src/task6.cpp
+++ src/task6.cpp
@@ -4,7 +4,7 @@ static unsigned long long fib(int N, unsigned long long *a, unsigned long long *
 {
 	unsigned long long c;
 	if (N == 1 || N == 2)
-		*a = 1, *b = 1;
+		*a = 0, *b = 1;
 	else
 	{
 		c = fib(N - 1, a, b);
