050370d8f693de2ae0ce1410951e2f52ac9a432c
krostis <44963059+krostis@users.noreply.github.com>
2019-02-28 19:40:16 +0300

    Update main8.cpp

--- src/main8.cpp
+++ src/main8.cpp
@@ -1,17 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "task8.h"
 
-int main(int argc, char* argv[])
+ int main(int argc, char* argv[])
 {
-	
-	//char buf [] = "(((7-1)/(4+2))-9)";
+	if (argc < 2)
+	{
+		printf("wrong format");
+		return 1;
+	}
+	printf("expression: %s\n", argv[1]);
+	char e1[SIZE] = { 0 }, e2[SIZE] = { 0 };
 	int res = eval(argv[1]);
-	printf("%d", res);
-
-	getchar();
+	printf("%d\n", res);
 	return 0;
 }
-
