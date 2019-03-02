f33843b3a0ca8334ba2878db1621994eaa6b9224
Anastasia Spiridonova <asb_gzas@mail.ru>
2019-03-03 01:12:46 +0300

    6 lab let's try again

--- src/task2.cpp
+++ src/task2.cpp
@@ -3,24 +3,20 @@
 unsigned int collatz(ULL num)
 {
 	if (num == 1)//basic task
-	{
 		return 1;
-	}
+
 	if (num % 2 != 0)
-	{
 		return collatz(num * 3 + 1) + 1;
-	}
 
 	else
-	{
 		return collatz(num / 2) + 1;
-	}
 }
 
 unsigned int seqCollatz(unsigned int *maxlen)
 //func, returning number and entering maximum length to maxlen
 {
-	unsigned int res = 0, currentLen = 0;
+	unsigned int currentLen = 0;
+	ULL num = 0, res = 0;
 	for (unsigned int num = 2; num <= 1000000; num++)
 	{
 		currentLen = collatz(num);
