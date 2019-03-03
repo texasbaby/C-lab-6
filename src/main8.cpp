898b03ed842024c6e3765306be8aecc0e8c10527
lerkas <44975578+lerkas@users.noreply.github.com>
2019-03-03 18:43:59 +0300

    Update main8.cpp

--- src/main8.cpp
+++ src/main8.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
         printf("Выражение отсутствует\n");
         return 1;
     }
-    else printf("Значение выражения: %s = %d\n", argv[1], eval(argv[1]));
+    else printf("Значение выражения: %d\n", eval(argv[1]));
 
     return 0;
 }
